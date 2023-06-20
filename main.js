53aefbf26183d47830b54239ba65e713e9a0e909
SebastianSoftware <56850980+SebastianSoftware@users.noreply.github.com>
2023-06-20 18:22:21 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+//Import Dependencies
+const {App, BrowserWindow} = require('Electron');
+const client = require('discord-rich-presence')('1120853911109120024');
+
+//Discord Rich Presence
+client.updatePresence({
+  state: 'Browsing',
+  startTimestamp: Date.now(),
+  largeImageKey: 'twitterlogo',
+  instance: true,
+});
+
+//Creating the Window
+const createWindow = () => {
+  //Define a new BrowserWindow
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 512,
+    icon: __dirname + '/favicon.png',
+    autoHideMenuBar: true,
+  })
+  win.loadURL('https://twitter.com');
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+  createWindow();
+});
